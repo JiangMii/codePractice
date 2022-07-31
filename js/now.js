06efb5782a0a7b4d3c28336a6a9197ed10773be2
蒋汨 <jiangmi18991428813@163.com>
2022-07-31 08:49:38 +0800

    比较版本号

--- js/now.js
+++ js/now.js
@@ -1,16 +1,36 @@
-var lengthOfLongestSubstring = function(s) {
-    let left=0,right=left+1;
-    let arr=s.slice(left,right);
-    let res=right-left;
-    while(right<s.length){
-    if(arr.indexOf(s[right])==-1){
-        right++;
-    }else{
-        left=right;
-        right=left+1;
-        res=Math.max(res,right-left);
+var compareVersion = function(version1, version2) {
+    let v1=version1.indexOf('.')==-1?[version1]:version1.split('.');
+    let v2=version2.indexOf('.')==-1?[version2]:version2.split('.');
+   // console.log(v1,v2)
+    let l=Math.max(v1.length,v2.length);
+    console.log(l)
+    let findZore=function(s){
+        let left=0;
+        while(left<s.length){
+           if(s[left]!==0){
+               return Number(s.slice(left));
+           }else{
+               left++;
+           }
+        }
+       
     }
-    return res;
+    for(let i=0;i<l;i++){
+        v1[i]=v1[i]?v1[i]:0, v2[i]=v2[i]?v2[i]:0;
+        let a=findZore(v1[i])?findZore(v1[i]):0;
+        let b=findZore(v2[i])?findZore(v2[i]):0;
+        console.log(a,b)
+    if(a>b){
+        console.log('s')
+        return 1;
+    }else if(a<b){
+        console.log('b')
+        return -1;
+    }
     
-    }}
-    
+    };
+    console.log('xt')
+    return 0
+    };
+compareVersion("1.0.1", "1");
+
